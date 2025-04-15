9fffeec1fb6b5792ebb9e2fe29a25933a9009130
rohit-web-tech <rohitdogra0126@gmail.com>
2025-04-15 21:50:56 +0530

    new changes

--- Backend/server.js
+++ Backend/server.js
@@ -94,6 +94,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  post: process.env.DB_PORT
 });
 
 // MySQL connection
